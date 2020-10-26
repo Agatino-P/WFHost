9f9d1b330426582ceea07225b2885e502398df91
Agatino Pesce <apesce@mecaldom.com>
2020-10-26 18:00:44 +0100

    Add project files.

---
+++ WFHost/ContentWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WFHost
+{
+    /// <summary>
+    /// Interaction logic for ContentWindow.xaml
+    /// </summary>
+    public partial class ContentWindow : Window
+    {
+        public ContentWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void SetContent(Object contentObj)
+        {
+            ccontrol.Content = contentObj;
+        }
+    }
+}
---
+++ WFHost/WFHControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WFHost
+{
+    /// <summary>
+    /// Interaction logic for WFHControl.xaml
+    /// </summary>
+    public partial class WFHControl : UserControl
+    {
+        public WFHControl()
+        {
+            InitializeComponent();
+
+            System.Windows.Forms.Button button = new System.Windows.Forms.Button();
+            button.Text = "wfbutton";
+            WFHost.Child = button;
+
+        }
+    }
+}
